591cf5c70267ef4c8ff342035babb3154ccdd93a
吉田優斗 <masato@yoshidayuutonoMacBook-Air.local>
2024-06-16 22:14:51 +0900

    修正

--- janken.rb
+++ janken.rb
@@ -10,7 +10,7 @@ loop do
         opponent_chois = rand(3).to_i
         if opponent_chois == 0
             puts "相手：グーを出しました"
-            puts "あいこです"
+            case_pattern = "draw"
         elsif opponent_chois == 1
             puts "相手：チョキを出しました"
             case_pattern = "you_win"
@@ -26,7 +26,7 @@ loop do
             case_pattern = "you_lose"
         elsif opponent_chois == 1
             puts "相手：チョキを出しました"
-            puts "あいこです"
+            case_pattern = "draw"
         elsif opponent_chois == 2
             puts "相手：パーを出しました"
             case_pattern = "you_win"
@@ -42,97 +42,120 @@ loop do
             case_pattern = "you_lose"
         elsif opponent_chois == 2
             puts "相手：パーを出しました"
-            puts "あいこです"
+            case_pattern = "draw"
         end
     elsif your_chois == 3
         case_pattern = "you_lose"
     else
         puts "0,1,2,3のいずれかを入力してください"
+        puts "---------------------------------------------"
     end
     case case_pattern
     when "you_win"
         puts "あなたの勝ちです"
-        puts "あなたの指す方向を選んでください"
-        puts "あっち向いて..."
-        puts "↑(0) ←(1) ↓(2)　→(3)"
-        your_select =gets.to_i
+        puts "---------------------------------------------"
+        your_select = ""
+        loop do
+          puts "あなたの指す方向を選んでください"
+          puts "あっち向いて..."
+          puts "↑(0) ←(1) ↓(2)　→(3)"
+          your_select = gets.to_i
+          if your_select == 0 || your_select == 1 ||
+             your_select == 2 || your_select == 3
+              break
+          else
+              puts "不正な値です"
+              puts "---------------------------------------------"
+          end
+        end
         puts "ホイ！"
         puts "---------------------------------------------"
         opponent_select = rand(4)
-        if your_select == 0
-            puts "あなた:↑を選びました"
-            if opponent_select == 0
-                puts "相手:↑を選びました"
-                puts "あなたの勝ちです　おめでとうございます！"
-                break
-            elsif opponent_select == 1
-                puts  "相手:←を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 2
-                puts "相手:↓を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 3
-                puts "相手:→を選びました"
-                puts "---------------------------------------------"
-            end
-        elsif your_select == 1
-            puts "あなた:←を選びました"
-            if opponent_select == 0
-                puts "相手:↑を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 1
-                puts  "相手:←を選びました"
-                puts "あなたの勝ちです　おめでとうございます！"
-                break
-            elsif opponent_select == 2
-                puts "相手:↓を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 3
-                puts "相手:→を選びました"
-                puts "---------------------------------------------"
-            end
-        elsif your_select == 2
-            puts "あなた:↓を選びました"
-            if opponent_select == 0
-                puts "相手:↑を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 1
-                puts  "相手:←を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 2
-                puts "相手:↓を選びました"
-                puts "あなたの勝ちです　おめでとうございます！"
-                break
-            elsif opponent_select == 3
-                puts "相手:→を選びました"
-                puts "---------------------------------------------"
-            end
-        elsif your_select == 3
-            puts "あなた:→を選びました"
-            if opponent_select == 0
-                puts "相手:↑を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 1
-                puts  "相手:←を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 2
-                puts "相手:↓を選びました"
-                puts "---------------------------------------------"
-            elsif opponent_select == 3
-                puts "相手:→を選びました"
-                puts "あなたの勝ちです　おめでとうございます！"
-                break
-            end
-        else
-            puts "不正な値です"
-            puts "---------------------------------------------"
-        end
+          if your_select == 0
+              puts "あなた:↑を選びました"
+              if opponent_select == 0
+                  puts "相手:↑を選びました"
+                  puts "あなたの勝ちです　おめでとうございます！"
+                  break
+              elsif opponent_select == 1
+                  puts  "相手:←を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 2
+                  puts "相手:↓を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 3
+                  puts "相手:→を選びました"
+                  puts "---------------------------------------------"
+              end
+          elsif your_select == 1
+              puts "あなた:←を選びました"
+              if opponent_select == 0
+                  puts "相手:↑を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 1
+                  puts  "相手:←を選びました"
+                  puts "あなたの勝ちです　おめでとうございます！"
+                  break
+              elsif opponent_select == 2
+                  puts "相手:↓を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 3
+                  puts "相手:→を選びました"
+                  puts "---------------------------------------------"
+              end
+          elsif your_select == 2
+              puts "あなた:↓を選びました"
+              if opponent_select == 0
+                  puts "相手:↑を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 1
+                  puts  "相手:←を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 2
+                  puts "相手:↓を選びました"
+                  puts "あなたの勝ちです　おめでとうございます！"
+                  break
+              elsif opponent_select == 3
+                  puts "相手:→を選びました"
+                  puts "---------------------------------------------"
+              end
+          elsif your_select == 3
+              puts "あなた:→を選びました"
+              if opponent_select == 0
+                  puts "相手:↑を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 1
+                  puts  "相手:←を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 2
+                  puts "相手:↓を選びました"
+                  puts "---------------------------------------------"
+              elsif opponent_select == 3
+                  puts "相手:→を選びました"
+                  puts "あなたの勝ちです　おめでとうございます！"
+                  break
+              end
+          end
+    when "draw"
+        puts "あいこです"
+        puts "---------------------------------------------"
     when "you_lose"
         puts "あなたの負けです"
-        puts "あなたの向く方向を選んでください"
-        puts "あっち向いて..."
-        puts "↑(0) ←(1) ↓(2)　→(3)"
-        your_select =gets.to_i
+        puts "---------------------------------------------"
+        your_select = ""
+        loop do
+          puts "あなたの向く方向を選んでください"
+          puts "あっち向いて..."
+          puts "↑(0) ←(1) ↓(2)　→(3)"
+          your_select =gets.to_i
+          if your_select == 0 || your_select == 1 ||
+             your_select == 2 || your_select == 3
+              break
+          else
+            puts "不正な値です"
+            puts "---------------------------------------------"
+          end
+        end
         puts "ホイ！"
         puts "---------------------------------------------"
         opponent_select = rand(4)
@@ -200,9 +223,6 @@ loop do
                 puts "あなたの負けです　残念でした"
                 break
             end
-        else
-            puts "不正な値です"
-            puts "---------------------------------------------"
         end
     end       
 end
